9614c1c11daec0ac7f68fada7dea8f2a4401b5d3
arthur10vieira <arthur10vieira@gmail.com>
2026-01-11 22:51:14 -0300

    Etapa 9 completada

---
+++ src/database/models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+
+        password: {
+          type: DataTypes.VIRTUAL,
+          allowNull: false,
+        },
+
+        password_hash: DataTypes.STRING,
+      },
+      {
+        sequelize,
+        tableName: 'users',
+      }
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 10);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+module.exports = User;
